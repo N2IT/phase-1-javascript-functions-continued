92450f9c759169a7da64c21644acea007088c798
Tony Eder <tonyeder11@gmail.com>
2023-10-11 13:17:16 -0700

    the last one was tough!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(item = "roller-skate") {
+    return (`This Saturday, I want to ${item}!`);
+}
+
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+
+
+// I NEED EXPLANATION HERE
+function wrapAdjective(signal = ('*')) {
+    return function(param1 = "special") {
+        return `You are ${signal}${param1}${signal}!`;
+    }
+}
+
+
+// function wrapAdjective(symbol = '*') {
+//     return function(adjective) {
+//         return `You are ${symbol}${adjective}${symbol}!`;
+//     }
+// }
+
+// function wrapAdjective(symbol = '||') {
+//     return function(adjective) {
+//         return `You are ${symbol}${adjective}${symbol}!`;
+//     }
+// }
+// EXPLANATION TO HERE
+
+
